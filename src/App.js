7a87c8d615a6a57254f53cf31cf6eaa44104fb93
bruhboy <78496518+ChochuBaba@users.noreply.github.com>
2024-06-22 00:56:30 +0530

    port change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+  import React, { useState } from 'react';
+  import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [avg, setAvg]=useState(1);
 
-export default App;
+    useEffect(() => 
+      {
+        let intervalId;
+
+        if (isActive) {
+            intervalId = setInterval(() => {
+                setTime(prevTime => prevTime + 1);
+            }, 1000);
+        } else {
+            clearInterval(intervalId);
+        }
+
+          return () => clearInterval(intervalId);
+      }, [isActive]);
+
+    const handleStart = () => {
+        setAvg(1);
+        setIsActive(true);
+    };
+
+    const handleStop = () => {
+        setIsActive(false);
+    };
+
+    const handleReset = async () => {
+        setAvg(time/count)
+        setIsActive(false);
+        
+        setTime(0);
+        setCount(0);
+    };
+
+    return (
+      <div>
+      <h1 style={{ color: isActive ? 'green' : 'red' }}>Timer: {time} seconds</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      <br></br>
+      <button style={{ fontSize: '12rem', color:'blue' }} onClick={() => setCount(count + 1)}>
+        tap
+      </button>
+        <h2>Average is : {avg}</h2>
+      </div>
+    );
+  }
+
+  export default App;
